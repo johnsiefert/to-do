5b7c50ed62f6e85282adef2dda7fbaba277c638e
John <johnsief12@gmail.com>
2023-10-23 17:19:41 -0400

    added delete button and new item adds in a delete button

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-let button = document.getElementById("enter");
+let enter = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
-let li = document.querySelectorAll("li")
+let li = document.querySelectorAll("li");
+
+for (let i = 0; i < li.length; i++) {
+	li[i].addEventListener("click", toggle);
+  }
+
+  for(let i=0; i< del.length; i++){
+	del[i].addEventListener("click", deleteListElement);
+}
+
+
+function deleteListElement(){
+	this.parentNode.parentNode.removeChild(this.parentNode);
+  }
 
 function inputLength() {
 	return input.value.length;
-}
-
-function createListElement() {
-	let li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
 }
 
 function addListAfterClick() {
@@ -26,14 +32,31 @@ function addListAfterKeydown(event) {
 	}
 }
 
-for (let i = 0; i < li.length; i++) {
-	li[i].addEventListener("click", toggle);
-  }
 
   function toggle() {
 	this.classList.toggle("done");
   }
 
-button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keydown", addListAfterKeypress);
+  function createListElement() {
+	if(!input.value.trim()){
+		alert('You must enter a value');
+		return;
+	  }
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	let delbutton= document.createElement("button");
+	delbutton.appendChild(document.createTextNode("x"));
+	li.appendChild(delbutton);
+	ul.appendChild(li);
+
+	delbutton.addEventListener("click", deleteListElement);
+	li.addEventListener("click", toggle);
+
+}
+
+enter.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keydown", addListAfterKeydown);
